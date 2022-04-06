2e2480b11dd7683241a7287bccd7a351fa34defd
zhihaozheng <zzhengcn@gmail.com>
2022-04-06 13:55:13 -0400

    test local convert

--- convert/__init__.py
+++ convert/__init__.py
@@ -8,6 +8,7 @@ import pathos.pools
 from tqdm import tqdm
 
 from cloudfiles import CloudFiles
+from cloudvolume.lib import mkdir, touch
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
@@ -21,13 +22,50 @@ def main(ctx, parallel):
     ctx.ensure_object(dict)
     ctx.obj["parallel"] = max(min(parallel, mp.cpu_count()), 1)
 
+@main.command()
+@click.argument("source")
+@click.argument("destination")
+@click.pass_context
+def to_tif(ctx, source, destination):
+    tile_path = os.path.join(source,"subtiles")
+
+    progress_dir = mkdir(os.path.join(tile_path, 'progress_tif'))
+
+    done_files = set(os.listdir(progress_dir))
+
+    all_files = os.listdir(tile_path)
+
+    all_files = set([
+            fname for fname in all_files
+            if os.path.splitext(fname)[1] == ".bmp"
+        ])
+
+    to_cv = list(all_files.difference(done_files))
+    to_cv.sort()
+
+    target_path = os.path.join(destination, souce.split("/")[-1])
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+
+    def process(filename):
+        img = cv2.imread(os.path.join(tile_path,filename),cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite(os.path.join(target_path, filename.replace("bmp","tif")), img)
+        return 1
+
+    parallel = int(ctx.obj.get("parallel", 1))
+
+    with tqdm(desc="Convert", total=len(all_files), initial=len(done_files)) as pbar:
+        with pathos.pools.ProcessPool(parallel) as pool:
+            for num_inserted in pool.imap(process, to_cv):
+                pbar.update(num_inserted)
+
 
 @main.command()
 @click.argument("source")
 @click.argument("destination")
 @click.pass_context
-def dl_to_tif(ctx, source, destination):
-    """.
+def download_to_tif(ctx, source, destination):
+    """
     """
     cf = CloudFiles(source, progress=True)
 
@@ -42,17 +80,8 @@ def dl_to_tif(ctx, source, destination):
         os.makedirs(destination)
 
     def process(filename):
-
-
         img = cf.get(['file1', 'file2', 'file3', ..., 'fileN'])
-
-        cv2.
-
-
-            cv2.imwrite(os.path.join(destination, f.replace("bmp","tif")), img)
-
-
-
+        cv2.imwrite(os.path.join(destination, f.replace("bmp","tif")), img)
         return 1
 
     parallel = int(ctx.obj.get("parallel", 1))
