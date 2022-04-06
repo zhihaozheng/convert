b77374cab019e1e77f51f4afbfd9ead4278ff5d1
zhihaozheng <zzhengcn@gmail.com>
2022-04-05 23:44:09 -0400

    switch to script

--- convert/__init__.py
+++ convert/__init__.py
@@ -9,9 +9,6 @@ from tqdm import tqdm
 
 from cloudfiles import CloudFiles
 
-
-
-
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
 @click.pass_context
@@ -29,42 +26,38 @@ def main(ctx, parallel):
 @click.argument("source")
 @click.argument("destination")
 @click.pass_context
-def to_tif(ctx, source, destination):
+def dl_to_tif(ctx, source, destination):
     """.
     """
     cf = CloudFiles(source, progress=True)
 
-
-    vol = CloudVolume(destination)
-    progress_dir = mkdir(os.path.join(source, 'progress'))
-
-    done_files = set(os.listdir(progress_dir))
-    all_files = os.listdir(source)
     all_files = set([
-        fname for fname in all_files
-        if (
-            os.path.isfile(os.path.join(source, fname))
-            and os.path.splitext(fname)[1] == ".bmp"
-        )
-    ])
-    to_upload = list(all_files.difference(done_files))
-    to_upload.sort()
+        fname for fname in cf.list(prefix='abc123'):
+
+        if split(fname)[1] == ".bmp"
+        ])
+    to_download = list(all_files)
+    to_download.sort()
+    if not os.path.exists(destination):
+        os.makedirs(destination)
 
     def process(filename):
 
-        img = cv2.imread(os.path.join(source, filename), cv2.IMREAD_GRAYSCALE)
-        img = cv2.transpose(img)
-        while img.ndim < 4:
-            img = img[..., np.newaxis]
 
-        bbx = Bbox.from_filename(get_ng(filename, z=z))
-        vol[bbx] = img
-        touch(os.path.join(progress_dir, filename))
+        img = cf.get(['file1', 'file2', 'file3', ..., 'fileN'])
+
+        cv2.
+
+
+            cv2.imwrite(os.path.join(destination, f.replace("bmp","tif")), img)
+
+
+
         return 1
 
     parallel = int(ctx.obj.get("parallel", 1))
 
-    with tqdm(desc="Upload", total=len(all_files), initial=len(done_files)) as pbar:
+    with tqdm(desc="download", total=len(all_files)) as pbar:
         with pathos.pools.ProcessPool(parallel) as pool:
-            for num_inserted in pool.imap(process, to_upload):
+            for num_inserted in pool.imap(process, to_download):
                 pbar.update(num_inserted)
