6158845c95d8225d03d51daa59d7c620bf5eee73
zhihaozheng <zzhengcn@gmail.com>
2022-05-03 17:35:35 -0400

    add command to produce alignTK script

--- temu/__init__.py
+++ temu/__init__.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from cloudfiles import CloudFiles
 from cloudvolume.lib import mkdir, touch
 
-from temu.funs import get_pairs,get_good_pairs
+from temu.funs import get_pairs,get_good_pairs,get_region
 
 @click.group()
 @click.option("-p", "--parallel", default=1, help="Run with this number of parallel processes. If 0, use number of cores.")
@@ -122,6 +122,7 @@ def getgoodpairs(acq_label,tile_path,pos_path,save_path,exclude):
     acq_path: e.g. /media/voxa/WD_23/zhihao/ca3/tape3_blade2/211222/bladeseq-2021.12.24-14.55.36/s108-2021.12.24-14.55.36
     tile_path: e.g. /media/voxa/WD_36/zhihao/ca3/tape3_blade2_tif/s108-2021.12.24-14.55.36
     save_path: e.g. /media/voxa/WD_36/zhihao/ca3/tape3_blade2_maps
+    exclude: e.g.
 
     !! save_path assume dir structure like this
     save_path = "/media/voxa/WD_36/zhihao/ca3/tape3_blade2_maps/"
@@ -145,3 +146,62 @@ def getgoodpairs(acq_label,tile_path,pos_path,save_path,exclude):
             exclude=f.read().splitlines()
 
     get_good_pairs(acq_label,summary_f,tile_path,pos_path,lst_save_path,exclude,fname="core",corr_threshold=0.85)
+
+# example script: temu tk --img_dir --acq_label --output_dir get_tk --rst
+@main.group("tk")
+@click.argument('--img_dir', default="", required=True, help="directory to tif images, wo the last slash")
+@click.argument('--acq_label', default="", required=True, help="acq_label, e.g. s010")
+@click.argument('--output_dir', default="scripts", help="directory where alignTK script will be saved to")
+def tkgroup(ctx,img_dir, acq_label, output_dir):
+  """
+  produce alignTK script to run stitching
+  """
+  ctx.ensure_object(dict)
+  ctx.obj["img_dir"] = img_dir
+  ctx.obj["acq_label"] = acq_label
+  ctx.obj["output_dir"]= output_dir
+
+@tkgroup.command()
+@click.option('--rst', default=False, is_flag=True, help="")
+@click.option('--register', default=False, is_flag=True, help="")
+@click.option('--align', default=False, is_flag=True, help="")
+@click.option('--imap', default=False, is_flag=True, help="")
+@click.argument('--apply_map_red', default=None, type=str, help="output path, will append acq_label subdir")
+@click.argument('--apply_map_fullres', default=None, type=str, help="")
+@click.pass_context
+def get_tk(ctx,):
+    img_dir = ctx.object.get("img_dir")
+    acq = ctx.object.get("acq_label")
+    output_dir = ctx.object.get("output_dir")
+
+    txt_lst = []
+    if rst:
+        # acq, img_dir
+        txt_lst.append("mpirun -np 20 find_rst -pairs lst/{acq}_pairs.lst -tif -images {img_dir}/ -output cmaps/{acq}/ -max_res 2048 -scale 1.0 -summary cmaps/{acq}/summary.out -margin 6 -rotation 0 -tx -100-100 -ty -100-100 -trans_feature 8 -distortion 1.0;".format(acq=acq,img_dir=img_dir))
+    elif register:
+        txt_lst.append("mpirun -np 20 register -pairs lst/s{acq}_pairs.lst -images {img_dir}/ -output maps/s{acq}/ -initial_map cmaps/s{acq}/ -distortion 13.0 -output_level 7 -depth 6 -quality 0.1 -summary maps/s{acq}/summary.out -min_overlap 10.0;".format(acq=acq,img_dir=img_dir))
+    elif align:
+        txt_lst.append("mpirun -np 22 align -images {img_dir}/ -image_list lst/{acq}_core_images.lst -maps maps/{acq}/ -map_list lst/{acq}_core_pairs.lst -output amaps/{acq}/ -schedule schedule_1.lst -incremental -output_grid grids/{acq}/ -grid_size 8192x8192 -fold_recovery 360;".format(acq=acq,img_dir=img_dir))
+    elif apply_map_red:
+    # acq, img_dir, output_dir
+        txt_lst.append("apply_map -image_list lst/{acq}_core_images.lst -images {img_dir}/ -maps amaps/{acq}/ -output {odir}/{acq}/ -memory 7000 -overlay -rotation -30 -rotation_center 20000,0 --reduction 16;".format(acq=acq,img_dir=img_dir, odir=apply_map_red))
+    elif imap:
+    # acq, img_dir
+        txt_lst.append("gen_imaps -image_list lst/{acq}_core_images.lst -images {img_dir}/ -map_list lst/{acq}_core_pairs.lst -output imaps/{acq}/ -maps maps/{acq}/;".format(acq=acq,img_dir=img_dir))
+    with open(output_dir,"w") as f:
+        f.write("".join(txt_lst))
+
+
+@tkgroup.command()
+@click.argument('size_path')
+@click.argument('apply_map_dir')
+@click.pass_context
+def get_apply_map_fullres(ctx,):
+    img_dir = ctx.object.get("img_dir")
+    acq = ctx.object.get("acq_label")
+    output_dir = ctx.object.get("output_dir")
+    # acq, img_dir, size_path,
+    with open(output_dir,"w") as f:
+        f.write(
+        "apply_map -image_list lst/{acq}_core_images.lst -images {img_dir}/ -maps amaps/{acq}/ -output {odir} -memory 7000 -overlay -rotation -30 -rotation_center 20000,0 -imaps imaps/{acq}/ -tile 2048x2048 -region ".format(acq=acq, img_dir=img_dir, odir=apply_map_dir) + funs.get_region(size_path)
+        )
